22919a5690373ed33df27d516b5910116ea0230e
simonlu <441509577@qq.com>
2023-08-17 16:55:12 +0800

    Update DouyinCourseService.java

--- DouyinCourseService.java
+++ DouyinCourseService.java
@@ -134,8 +134,8 @@ public class DouyinCourseService {
             return accessToken;
         }
         JSONObject jsonObject = JSONObject.parseObject("{\n" +
-                "  \"appid\": \"tt8d0ffbb2123003fd01\",\n" +
-                "  \"secret\": \"8a25026836379d573092ec6cd2b99a17d46f797e\",\n" +
+                "  \"appid\": \"appid\",\n" +
+                "  \"secret\": \"secret\",\n" +
                 "  \"grant_type\": \"client_credential\"\n" +
                 "}");
 
@@ -148,7 +148,7 @@ public class DouyinCourseService {
         String token = getAccessToken();
         JSONObject jsonObject = JSONObject.parseObject("{\n" +
                 "  \"access_token\": \"0801121846735561486f5a48744d744157456a554a68446d52773d3d\",\n" +
-                "  \"appid\": \"tt8d0ffbb2123003fd01\",\n" +
+                "  \"appid\": \"appid\",\n" +
                 "  \"resource_uri\": \"product/resource/abcd\"\n" +
                 "}");
         jsonObject.put("access_token",token);
@@ -164,7 +164,7 @@ public class DouyinCourseService {
             String token = getAccessToken();
             JSONObject jsonObject = JSONObject.parseObject("{\n" +
                     "  \"access_token\": \"0801121846336c634975494f592f5973475a4242493756754d513d3d\",\n" +
-                    "  \"appid\": \"tt8d0ffbb2123003fd01\",\n" +
+                    "  \"appid\": \"appid\",\n" +
                     "  \"resource_type\": 2,\n" +
                     "  \"resource_url\": \"http://vod.vipflonline.com/922011a4vodcq1300677316/78e4bf77243791581366744219/4b8b43v8pa0A.mp4?t=64d36694&us=72d4cd1101&sign=1543b81d566ad6cb965142c17e0ee44f\"" +
                     "}");
@@ -250,51 +250,7 @@ public class DouyinCourseService {
 
     }
 
-    public static void readImg(){
-        String sql = "update course set detail='%s',update_time=now() where id = '%s';";
-        List<String> fileNames = FileUtil.listFileNames("C:\\Users\\mac\\Pictures\\课程新图");
-        fileNames.forEach(file->{
-            System.out.println("\""+file.split("\\.")[0]+"\",");
-           // System.out.println(String.format(sql,"/course_detail_new/"+file,file.split("\\.")[0]));
-        });
-    }
+   
 
-    public static  void readProductId(){
-        String json = FileUtil.readUtf8String("C:\\Users\\mac\\Downloads\\product.txt");
-        JSONObject jsonObject = JSONObject.parseObject(json);
-        JSONObject data = jsonObject.getJSONObject("data");
-        String sql = "update course set douyin_course_id = \"%s\",update_time=now() where id = \"%s\";";
-        data.forEach((new BiConsumer<String, Object>() {
-            @Override
-            public void accept(String s, Object o) {
-                System.out.println(String.format(sql,o,s));
-                //System.out.println(o+",");
-            }
-        }));
-    }
-
-    public static void main(String[] args) {
-       readProductId();
-        //String result =  "已进审课程7266291344409690173".replaceAll("[^\\d]+","");
-       //System.out.println(result);
-       // readImg();
-//        DouyinCourseService service = new DouyinCourseService();
-//        DouyinCourseAddRequest request = new DouyinCourseAddRequest();
-//        request.setCourseId("ff8080817de62318017e75681cf705f6");
-//        request.setName("雅思冲刺班-99%的学霸都在用的高分神器");
-//        request.setSummary("本课程针对国内雅思考生常见问题和误区（答题慢，看不懂，没有考点思维等），结合我这10年的经验总结，以出题人思维角度，从能力、思维、方法、技巧、练习5个维度，帮你突破思维局限，迅速拿到目标分数。\n" +
-//                "共14节思维方法技巧课，深度系统化讲解雅思阅读考试所有题型考点、方法、技巧。18节实战特训课，重难点文章主题精讲，带你在实战中灵活运用方法技巧。课程中所讲方法都经过我本人和大量考生实战验证过，适用国内外雅思考试。");
-//        request.setPrice(BigDecimal.valueOf(388));
-//        request.setDetail("https://flo-test-1300677316.cos.ap-guangzhou.myqcloud.com//course_detail_lines/clfh5nVXjDnAjvPW1309DmqkX0DXdgkj.jpg");
-//
-//        DouyinCourseMediaAddRequest mediaAddRequest = new DouyinCourseMediaAddRequest();
-//
-//        mediaAddRequest.setUri("http://vod.vipflonline.com/922011a4vodcq1300677316/ee524116387702294851454903/FDvmc1HJl9gA.mp4?t=64d72e4c&us=72d4cd1101&sign=f329b6cbef3d8222ea7ad37dc093341d");
-//        mediaAddRequest.setType(2);
-//        mediaAddRequest.setName("第1课时");
-//        mediaAddRequest.setText("把握规律，3个角度搞清出题人思维");
-//        request.setPeriods(List.of(mediaAddRequest));
-//        String productId = service.uploadCourse(request);
-//        log.debug(productId);
-    }
+   
 }
